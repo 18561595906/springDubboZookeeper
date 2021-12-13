b7feeabff716362a381648b63a800330f52381e7
18561595906 <18561595906@163.com>
2021-12-13 11:18:40 +0800

    #新增redis的配置  #11点18分

--- dubbo-demo-service/src/main/java/com/moy/maven/service/impl/UserServiceImpl.java
+++ dubbo-demo-service/src/main/java/com/moy/maven/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.moy.maven.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.moy.maven.domain.User;
 import org.moy.maven.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.moy.maven.mapper.UserMapper;
@@ -14,9 +17,18 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Override
     public List<User> findAll() {
+        redisTemplate.opsForHash().put("map1_data5", "A", "A123");
+        redisTemplate.opsForHash().put("map1_data5", "B", "B123");
+    	redisTemplate.opsForValue().set("qq5", "99999999999999999999999");
+    	String obj = (String) redisTemplate.opsForValue().get("qq5");
+    	System.out.println("String的value------------"+obj);
+    	Map<String,String> objHash = redisTemplate.boundHashOps("map1_data5").entries();
+    	System.out.println("Hash的value------------"+objHash);
         return userMapper.select(null);
     }
 
